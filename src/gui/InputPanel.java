9b1b288a65bf081d060259ab17106e41ce91e13e
Lucia Martos <lm260@duke.edu>
2016-10-15 22:11:47 -0400

    more work on the basic starting GUI

--- src/gui/InputPanel.java
+++ src/gui/InputPanel.java
@@ -2,36 +2,52 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class InputPanel extends HBox {
 
-    private Button gameSelectButton;
-    private Button viewToggleButton;
+	private static final int PADDING = 15;
+	private static int RUN_BUTTON_WIDTH = 50;
+	private static int INPUT_PANEL_PADDING = 10;
+	private Button runButton;
+	private TextField commandInput;
+	private String currentCommandLine;
 
-    public InputPanel(double height, EventHandler<ActionEvent> gameSelectHandler,
-                       EventHandler<ActionEvent> viewToggleHandler) {
-        super(AppResources.INPUT_PANEL_PADDING.getDoubleResource());
-        setPrefHeight(height);
+	// TODO:check if there is a better way to get the width and the height
+	// rather than feeding it
+	public InputPanel(double height, double appWidth, double appHeight, EventHandler<ActionEvent> runCommandHandler) {
+		super(INPUT_PANEL_PADDING);
+		setPrefHeight(height);
+		setLayoutY(appHeight - height - PADDING);
+		setLayoutX(PADDING);
+		
+		// set the text box
+		double width = appWidth - RUN_BUTTON_WIDTH- (PADDING * 2);
+		commandInput = new TextField();
+		commandInput.setPromptText("Enter your command here...");
+		commandInput.setPrefWidth(width); 	//CHANGE TO COMMAND BUTTON WIDTH
+		commandInput.getText();
 
-        gameSelectButton = new Button(AppResources.GAME_SELECT_TITLE.getResource());
-        gameSelectButton.setPrefWidth(AppResources.INPUT_BUTTON_WIDTH.getDoubleResource());
-        gameSelectButton.setOnAction(gameSelectHandler);
+		//set the run button
+		runButton = new Button("Run");
+		runButton.setPrefWidth(RUN_BUTTON_WIDTH);
+		runButton.setOnAction(event ->{
+			runCommandHandler.handle(event);
+				currentCommandLine = commandInput.getText();
+				System.out.println(currentCommandLine);
+				commandInput.clear();
+		});
 
-        viewToggleButton = new Button(AppResources.CHART_VIEW_TITLE.getResource());
-        viewToggleButton.setPrefWidth(AppResources.INPUT_BUTTON_WIDTH.getDoubleResource());
-        viewToggleButton.setOnAction(event -> {
-            viewToggleHandler.handle(event);
-            if (viewToggleButton.getText().equals(AppResources.CHART_VIEW_TITLE.getResource())) {
-                viewToggleButton.setText(AppResources.GRID_VIEW_TITLE.getResource());
-            } else {
-                viewToggleButton.setText(AppResources.CHART_VIEW_TITLE.getResource());
-            }
-        });
-
-        getChildren().addAll(gameSelectButton, viewToggleButton);
-    }
+		getChildren().addAll(commandInput, runButton);
+	}
+	
+	//Is this good design..?
+	public String getCurrentCommandLine(){
+		return currentCommandLine;
+	}
 
 }
--- src/gui/MainView.java
+++ src/gui/MainView.java
@@ -1,15 +1,15 @@
 package gui;
 
 import java.util.List;
-
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
 import javafx.scene.paint.Color;
 
 public class MainView {
@@ -18,29 +18,34 @@ public class MainView {
 	private static final double APP_WIDTH = 600;
 	private static final double APP_HEIGHT = 600;
 	private static final double INPUT_PANEL_HEIGHT = 20;
+	private static final double PAST_COMMAND_LIST_WIDTH = 120;
 	private Group sceneRoot;
 	private Scene scene;
 	private double appWidth, appHeight;
 	private TitleBox titleBox;
+	private InputPanel inputPanel;
+	private ListView<String> myListPastCommands;
+	private ObservableList<String> pastCommands;
 
 	public MainView() {
 		sceneRoot = new Group();
-		appWidth = APP_WIDTH; 
-		appHeight = APP_HEIGHT; 
+		appWidth = APP_WIDTH;
+		appHeight = APP_HEIGHT;
 		scene = new Scene(sceneRoot, appWidth, appHeight);
 		sceneRoot.setId("root");
 
 		createTitleBox();
 		createDrawingGrid();
 		createCommandInputter();
+		createListPastCommands();
 
 	}
 
 	private void createTitleBox() {
-		double x = PADDING; 
+		double x = PADDING;
 		double y = PADDING;
 		double width = appWidth - (2 * PADDING);
-		double height = TITLE_BOX_HEIGHT; 
+		double height = TITLE_BOX_HEIGHT;
 		String title = "SLOGO";
 		titleBox = new TitleBox(x, y, width, height, title);
 		sceneRoot.getChildren().add(titleBox);
@@ -48,7 +53,7 @@ public class MainView {
 
 	private void createDrawingGrid() {
 		Canvas canvas = new Canvas(400, 200);
-		canvas.setWidth(appWidth - PADDING);
+		canvas.setWidth(appWidth - PADDING - PAST_COMMAND_LIST_WIDTH);
 		canvas.setHeight(appHeight - PADDING - TITLE_BOX_HEIGHT);
 		canvas.setLayoutX(PADDING);
 		canvas.setLayoutY(PADDING + TITLE_BOX_HEIGHT);
@@ -60,25 +65,34 @@ public class MainView {
 		gc.fillOval(100, 70, 50, 30);
 		sceneRoot.getChildren().add(canvas);
 	}
+	
+	private void createListPastCommands(){
+		myListPastCommands = new ListView<String>();
+		pastCommands = FXCollections.observableArrayList("Peras", "Manzanas");
+		myListPastCommands.setItems(pastCommands);
+		
+		myListPastCommands.setPrefWidth(PAST_COMMAND_LIST_WIDTH);
+		myListPastCommands.setPrefHeight(appHeight - INPUT_PANEL_HEIGHT - TITLE_BOX_HEIGHT - PADDING*4);
+		myListPastCommands.setLayoutX(appWidth - PADDING -PAST_COMMAND_LIST_WIDTH);
+		myListPastCommands.setLayoutY(PADDING*2 + TITLE_BOX_HEIGHT);
+
+		sceneRoot.getChildren().add(myListPastCommands);
+	}
 
 	private void createCommandInputter() {
-		//set the text box
-		double width = appWidth - (PADDING * 2);
-		double height = INPUT_PANEL_HEIGHT;
-		final TextField commandInput = new TextField();
-		commandInput.setPromptText("Enter your command here...");
-		commandInput.setPrefWidth(width);
-		commandInput.setPrefHeight(height);
-		commandInput.setLayoutY(appHeight - height - PADDING);
-		commandInput.setLayoutX(PADDING);
-		commandInput.getText();
-		
-		//set the run button
-		Button runButton = new Button("Run");
+		//TODO: Fix delay, it runs one command delayed how can i make the commadline accessible before?
+		EventHandler<ActionEvent> runCommandHandler = event -> {
+			String currentCommandLine = inputPanel.getCurrentCommandLine();
+			if (currentCommandLine == null || currentCommandLine.length() == 0) {
+				System.out.println("Return Error: EMPTY STRING");
+			} else {
+				System.out.println("RUN COMMAND: " + currentCommandLine);
+				pastCommands.add(currentCommandLine);
+			}
+		};
 
-		sceneRoot.getChildren().addAll(commandInput, runButton);
-		
-		
+		inputPanel = new InputPanel(INPUT_PANEL_HEIGHT, appHeight, appWidth, runCommandHandler);
+		sceneRoot.getChildren().addAll(inputPanel);
 	}
 
 	public Scene getScene() {
