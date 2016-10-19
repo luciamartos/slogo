89e1e9788252aba6c09d93d711d7fb205afeb9e3
Ray Song <ys101@duke.edu>
2016-10-19 17:53:00 -0400

    Added changes to interpreter

---
+++ src/interpreter/TurtleQueryDataSource.java
@@ -0,0 +1,9 @@
+package interpreter;
+
+public interface TurtleQueryDataSource {
+	public double getXCoordinate();
+	public double getYCoordinate();
+	public double getAngle();
+	public boolean getTurtleIsShowing();
+	public boolean getPenIsUp();
+}
--- src/interpreter/TurtleQueryInterpreter.java
+++ src/interpreter/TurtleQueryInterpreter.java
@@ -1,5 +1,6 @@
 package interpreter;
 
-public class TurtleQueryInterpreter {
-
+public class TurtleQueryInterpreter{
+	private static TurtleQueryDataSource dataSource;
+	
 }
