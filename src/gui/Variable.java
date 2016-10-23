a24c3403e6d19a77de76794e5016d8fe410f4d83
Eric Song <ess42@duke.edu>
2016-10-23 14:34:48 -0400

    hooked up environment variables

---
+++ src/gui/Variable.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Variable {
+	private final SimpleStringProperty name;
+    private final SimpleStringProperty value;
+ 
+    public Variable(String name, String value) {
+        this.name = new SimpleStringProperty(name);
+        this.value = new SimpleStringProperty(value);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+    public void setName(String name) {
+        this.name.set(name);
+    }
+        
+    public String getValue() {
+        return value.get();
+    }
+    public void setValue(String value) {
+        this.value.set(value);
+    }
+    
+    
+
+}
