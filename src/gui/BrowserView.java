b1488bce08e9609bc782a4271c8a53ff8c80d8fe
Lucia Martos <lm260@duke.edu>
2016-10-23 21:03:38 -0400

    Get Help button

--- src/gui/BrowserView.java
+++ src/gui/BrowserView.java
@@ -13,12 +13,15 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
  
 public class BrowserView {
+	
+    private static final double WEB_WIDTH = 800;
+	private static final double WEB_HEIGHT = 500;
 
-    public  BrowserView (Stage stage) {
+	public  BrowserView (Stage stage) {
        // stage.getScene().add
         stage.setTitle("Web View");
-      stage.setWidth(300);
-      stage.setHeight(300);
+      stage.setWidth(WEB_WIDTH);
+      stage.setHeight(WEB_HEIGHT);
         Scene scene = new Scene(new Group());
 
         VBox root = new VBox();     
@@ -26,25 +29,20 @@ public class BrowserView {
         final WebView browser = new WebView();
         final WebEngine webEngine = browser.getEngine();
 
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setContent(browser);
-        
         webEngine.getLoadWorker().stateProperty().addListener(
             new ChangeListener<State>() {
               @Override public void changed(ObservableValue ov, State oldState, State newState) {
 
                   if (newState == Worker.State.SUCCEEDED) {
                     stage.setTitle(webEngine.getLocation());
-                    System.out.println("called");
                 }
                   
                 }
             });
+        
         webEngine.load("http://www.cs.duke.edu/courses/compsci308/fall16/assign/03_slogo/commands.php");        
         
-        webEngine.loadContent("<b>asdf</b>");
-
-        root.getChildren().addAll(scrollPane);
+        root.getChildren().addAll(browser);
         scene.setRoot(root);
 
         stage.setScene(scene);
