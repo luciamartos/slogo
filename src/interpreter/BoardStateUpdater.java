f690d1b5a99148e178dcb25d0d7dd0de9f2e5d31
andrewbihl <andrewbihlva@gmail.com>
2016-10-31 18:29:40 -0400

    Update interpreter interfaces

--- src/interpreter/BoardStateUpdater.java
+++ src/interpreter/BoardStateUpdater.java
@@ -2,5 +2,6 @@ package interpreter;
 
 public interface BoardStateUpdater {
 	public void setBackgroundColorIndex(int i);
+	public void addColorToPalette(int index, int red, int green, int blue);
 	public void resetBoard();
 }
