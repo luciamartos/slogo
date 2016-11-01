234bd793a639bfe23cde84b4fd346e60fca1af19
andrewbihl <andrewbihlva@gmail.com>
2016-11-01 19:39:15 -0400

    interface change

--- src/gui/BoardActionsHandler.java
+++ src/gui/BoardActionsHandler.java
@@ -4,4 +4,5 @@ public interface BoardActionsHandler {
 	public void undo();
 	public void setBackgroundColor(int colorIndex);
 	public void loadBoardFromFile(String xmlFile);
+	public void saveStateToXMLFile(String fileName);
 }
