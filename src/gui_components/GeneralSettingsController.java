b019af5fb1f5673ffa95c58c15603bfe0155f8c3
Lucia Martos <lm260@duke.edu>
2016-11-02 09:06:20 -0400

    bugs

--- src/gui_components/GeneralSettingsController.java
+++ src/gui_components/GeneralSettingsController.java
@@ -20,7 +20,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import model.XMLReader;
 /**
  * @author Lucia Martos
  */
